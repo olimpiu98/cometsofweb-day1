3f863cc8e7223aabf4100e350c48cc4bd7c760f4
Olimpiu <ticudean.o@gmail.com>
2024-06-26 12:54:45 +0100

    Organizare fisiere.

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,8 +1,6 @@
-import "dotenv/config";
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
+const keypair = Keypair.generate();
 
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
-
-console.log(
-    `✅ Finished! We've loaded our keypair securely, using an env file! Our public key is: ${keypair.publicKey.toBase58()}`
-);
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
