1010a1277ee600acc672613d8623347b9f6aaadb
Olimpiu <ticudean.o@gmail.com>
2024-06-27 18:35:15 +0100

    Update cod pentru curs 2

--- load-keypair.ts
+++ load-keypair.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import bs58 from "bs58";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
-
+const privatKey = bs58.encode(keypair.secretKey);
 console.log(
-    `✅ Finished! We've loaded our keypair securely, using an env file! Our public key is: ${keypair.publicKey.toBase58()}`
+    `✅ Our public key is: ${keypair.publicKey.toBase58()}`
 );
+
+console.log(`🔐 Private key: ${privatKey}`);
